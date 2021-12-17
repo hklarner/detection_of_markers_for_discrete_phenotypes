7a8ea29491617a36b23c9fd9802e4176da1fa222
hklarner <leevilux@yahoo.de>
2021-12-17 18:27:13 +0100

    relaxes version dependency: uses >= instead of ==

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     version=read_version_txt(),
     packages=find_packages(),
     install_requires=[
-        "click==8.0.1",
-        "pydantic==1.8.2",
-        "pandas==1.1.0",
-        "pytest==6.0.1",
-        "clingo==5.5.0.post3",
+        "click>=8.0.1",
+        "pydantic>=1.8.2",
+        "pandas>=1.1.0",
+        "pytest>=6.0.1",
+        "clingo>=5.5.0.post3",
         "pyboolnet @ git+https://github.com/hklarner/pyboolnet"
     ],
     entry_points="""
